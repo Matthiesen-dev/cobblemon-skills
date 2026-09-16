3b69107a637060f22463a2f23e36da6ca822baff
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-16 05:07:24 -0700

    Add berry farming XP values to ExperienceMaps

--- common/src/main/java/dev/matthiesen/cobblemon_skills/common/interfaces/ExperienceMaps.java
+++ common/src/main/java/dev/matthiesen/cobblemon_skills/common/interfaces/ExperienceMaps.java
@@ -19,6 +19,7 @@ public final class ExperienceMaps {
     private static final Integer COBBLEMON_WOODCUTTING_XP = 80;
     private static final Integer COBBLEMON_WOODCUTTING_EXTRA_XP = 120;
     private static final Integer COBBLEMON_FARMING_XP = 150;
+    private static final Integer COBBLEMON_BERRY_FARMING_XP = 200;
 
     private static final Map<Block, Integer> COBBLEMON_ARCHEOLOGY_BLOCKS = Map.<Block, Integer>ofEntries(
             // Ores
@@ -84,7 +85,7 @@ public final class ExperienceMaps {
             Map.entry(CobblemonBlocks.RELIC_COIN_POUCH, RELIC_COIN_POUCH_XP)
     );
 
-    private static final Map<Block, Integer> COBBLEMON_BOTANY_BLOCKS = Map.ofEntries(
+    private static final Map<Block, Integer> COBBLEMON_BOTANY_BLOCKS = Map.<Block, Integer>ofEntries(
             Map.entry(CobblemonBlocks.APRICORN_LOG, COBBLEMON_WOODCUTTING_XP),
             Map.entry(CobblemonBlocks.APRICORN_LEAVES, COBBLEMON_WOODCUTTING_XP),
             Map.entry(CobblemonBlocks.SACCHARINE_LEAVES, COBBLEMON_WOODCUTTING_XP),
@@ -98,6 +99,59 @@ public final class ExperienceMaps {
             Map.entry(CobblemonBlocks.WHITE_APRICORN, COBBLEMON_FARMING_XP),
             Map.entry(CobblemonBlocks.YELLOW_APRICORN, COBBLEMON_FARMING_XP),
             Map.entry(CobblemonBlocks.HEARTY_GRAINS, COBBLEMON_FARMING_XP),
-            Map.entry(CobblemonBlocks.MEDICINAL_LEEK, COBBLEMON_FARMING_XP)
+            Map.entry(CobblemonBlocks.MEDICINAL_LEEK, COBBLEMON_FARMING_XP),
+
+            // Berries
+            Map.entry(CobblemonBlocks.AGUAV_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.APICOT_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.ASPEAR_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.BABIRI_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.BELUE_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.BLUK_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CHARTI_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CHERI_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CHESTO_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CHILAN_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CHOPLE_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.COBA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.COLBUR_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.CORNN_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.DURIN_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.ENIGMA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.FIGY_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.GANLON_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.GREPA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.HABAN_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.HONDEW_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.JABOCA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.KELPSY_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.LANSAT_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.LEPPA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.LIECHI_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.LUM_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.MARANGA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.MICLE_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.NANAB_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.NOMEL_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.OCCA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.ORAN_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.PASSHO_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.PAYAPA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.PERSIM_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.PETAYA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.PINAP_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.POMEG_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.QUALOT_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.RAZZ_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.RINDO_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.ROWAP_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.SALAC_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.SITRUS_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.STARF_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.TAMATO_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.TANGA_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.WATMEL_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.WIKI_BERRY, COBBLEMON_BERRY_FARMING_XP),
+            Map.entry(CobblemonBlocks.YACHE_BERRY, COBBLEMON_BERRY_FARMING_XP)
     );
 }
